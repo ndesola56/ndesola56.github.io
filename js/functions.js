9fe7a7bd1f1a8a97488ce389201b70186d2f4daf
nd1227 <nd1227@messiah.edu>
2019-05-02 00:16:53 -0400

    Initial commit of files

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+function submitFile() {        
+    localStorage.setItem("first", document.getElementById("first").value);
+    localStorage.setItem("last", document.getElementById("last").value);
+    localStorage.setItem("email", document.getElementById("email").value);
+    localStorage.setItem("linkedin", document.getElementById("linkedin").value);
+    localStorage.setItem("phone", document.getElementById("phone").value);
+    localStorage.setItem("grade", document.querySelector('input[name="grade"]:checked').value);
+    localStorage.setItem("major", document.getElementById("major").value);
+    localStorage.setItem("minor", document.getElementById("minor").value);
+    localStorage.setItem("concentration", document.getElementById("concentration").value);
+
+    var textarea = document.getElementById("experience").value;
+    textarea = textarea.split("\n");
+
+    localStorage.setItem("experience", JSON.stringify(textarea));
+
+    window.open('completedResume.html','_self',false);
+};
